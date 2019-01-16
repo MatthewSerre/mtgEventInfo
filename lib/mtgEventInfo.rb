62a179122e1a40d637c261ec24071540d13f5b54
matthew.serre <matthew.serre@gmail.com>
2019-01-16 19:16:16 +0000

    Added pry dependency.

--- lib/mtgEventInfo.rb
+++ lib/mtgEventInfo.rb
@@ -3,3 +3,4 @@ require_relative "./mtgEventInfo/CLI"
 require_relative "./mtgEventInfo/event"
 
 require "nokogiri"
+require "pry"
--- lib/mtgEventInfo/event.rb
+++ lib/mtgEventInfo/event.rb
@@ -5,21 +5,43 @@ class MtgEventInfo::Event
   def self.all
     # The scraper should create a new event using the scraped data in the form of a hash and send the key value pairs to create an object with all these properties that can then be added to an array and iterated through and accessed from other methods looking for particular details to sort and display.
     
-    event_1 = self.new
-    event_1.name = "SCG Open"
-    event_1.date = "1/11/13"
-    event_1.mtgFormat = "Standard"
-    event_1.location = "location"
-    event_1.registration_URL = "www.google.com"
+    # event_1 = self.new
+    # event_1.name = "SCG Open"
+    # event_1.date = "1/11/13"
+    # event_1.mtgFormat = "Standard"
+    # event_1.location = "location"
+    # event_1.registration_URL = "www.google.com"
     
-    event_2 = self.new
-    event_2.name = "MagicFest"
-    event_2.date = "2/2/12"
-    event_2.mtgFormat = "Modern"
-    event_2.location = "another location"
-    event_2.registration_URL = "www.wizards.com"
+    # event_2 = self.new
+    # event_2.name = "MagicFest"
+    # event_2.date = "2/2/12"
+    # event_2.mtgFormat = "Modern"
+    # event_2.location = "another location"
+    # event_2.registration_URL = "www.wizards.com"
     
-    [event_1, event_2]
+    # [event_1, event_2]
+    
+    self.scrape_events
+  end
+  
+  def self.scrape_events
+    events = []
+    
+    events << self.scrape_wizards
+    events << self.scrape_scg
+    
+    events
+  end
+  
+  def self.scrape_wizards
+    doc = Nokogiri::HTML(open("https://magic.wizards.com/en/content/premier-events-schedule#/overview"))
+    name = doc.search("span.info ng-binding").text
+    date = doc.search("h3.ng-binding").text
+    binding.pry
+  end
+  
+  def self.scrape_scg
+    
   end
 
   
