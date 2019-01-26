b976eacb64640c836cc7bcc3509e972a7f4dc0f7
matthew.serre <matthew.serre@gmail.com>
2019-01-26 02:58:21 +0000

    Changed source of data for scraping method.

--- lib/mtgEventInfo/event.rb
+++ lib/mtgEventInfo/event.rb
@@ -9,18 +9,28 @@ class MtgEventInfo::Event
   def self.scrape_events
     @events = []
     
-    # events << self.scrape_wizards
-    self.scrape_scg
+    self.scrape_cfb
+    # self.scrape_scg
     
     @events
   end
   
-  # def self.scrape_wizards
-  #   doc = Nokogiri::HTML(open("https://magic.wizards.com/en/content/premier-events-schedule#/list"))
-  #   name = doc.search("div.socials").text
-  #   date = doc.search("h3.ng-binding").text
-  #   # span.info.ng-binding
-  # end
+  def self.scrape_cfb
+    html = open("https://www.cfbevents.com/us-canada")
+    doc = Nokogiri::HTML.parse(html)
+    page_events = doc.css("div#comp-jmuqvt3rinlineContent-gridContainer")
+    page_events.each do |page_event|
+      event = Hash.new
+      event[:name] = page_event.css("span").text
+      # event[:location] = page_event.css("div.event-city").text
+      # event[:date] = page_event["id"]
+      # event[:moreInfoURL] = page_event.css("div.event-title a")[0]["href"]
+      # event[:mtgFormat] = page_event.css("div.main-event div.format-box a").text
+      # event[:TO] = "Channel Fireball"
+      @events.push(event)
+    end
+    # binding.pry
+  end
   
   def self.scrape_scg
     html = open("http://www.starcitygames.com/content/schedule")
@@ -33,6 +43,7 @@ class MtgEventInfo::Event
       event[:date] = page_event["id"]
       event[:moreInfoURL] = page_event.css("div.event-title a")[0]["href"]
       event[:mtgFormat] = page_event.css("div.main-event div.format-box a").text
+      event[:TO] = "Star City Games"
       @events.push(event)
     end
     # binding.pry
