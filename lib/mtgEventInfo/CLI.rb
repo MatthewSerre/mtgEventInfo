cfe7ea09db2668d61531e158e3606462c5a9362c
matthew.serre <matthew.serre@gmail.com>
2019-01-26 01:35:48 +0000

    Abstracted portion of CLI to another method and improved logic and flow of menu navigation.

--- lib/mtgEventInfo/CLI.rb
+++ lib/mtgEventInfo/CLI.rb
@@ -1,7 +1,7 @@
 class MtgEventInfo::CLI
   
   def call
-    puts "Welcome to the command line interface for the Magic: The Gathering Event Information tool."
+    puts "Welcome to Mox Ruby, a Magic: The Gathering Event Information tool."
     menu
     goodbye
   end
@@ -15,36 +15,50 @@ class MtgEventInfo::CLI
     @events = MtgEventInfo::Event.all
   end
   
+  def list_events_by_date
+    @events.each.with_index(1) do |event, i|
+      puts "#{i}. #{event[:date]} - #{event[:name]} - #{event[:location]}"
+    end
+  end
+  
+  def submenu
+    @events.each.with_index(1) do |event, i|
+      puts "#{i}. #{event[:date]} - #{event[:name]} - #{event[:location]}"
+    end
+  end
+  
   def menu
+    
     input = nil
     while input != "exit"
       list_events_by
       puts "Please enter the number corresponding to the option by which you want to sort and display upcoming events or enter 'exit'."
       input = gets.strip.downcase
+      
       case input
       when "1"
-        date_events = @events
-        date_events.each.with_index(1) do |event, i|
-          puts "#{i}. #{event[:date]} - #{event[:name]} - #{event[:location]}"
-        end
+        list_events_by_date
         date_input = nil
         while date_input != "back"
-          puts "Please enter the number corresponding to the event about which you would like more information or enter 'back'."
+          puts "Please enter the number corresponding to the event about which you would like more information or enter 'list' to see the list of events again or enter 'back' to return to the main menu."
           date_input = gets.strip.downcase
           if date_input.to_i > 0  && date_input.to_i <= @events.length
             puts ""
-            puts "#{date_events[date_input.to_i-1][:location]} #{date_events[date_input.to_i-1][:mtgFormat]} #{date_events[date_input.to_i-1][:name]}"
-              puts date_events[date_input.to_i-1][:date]
-              puts "#{date_events[date_input.to_i-1][:moreInfoURL]}"
+            puts "#{@events[date_input.to_i-1][:location]} #{@events[date_input.to_i-1][:mtgFormat]} #{@events[date_input.to_i-1][:name]}"
+              puts @events[date_input.to_i-1][:date]
+              puts "#{@events[date_input.to_i-1][:moreInfoURL]}"
             puts ""
           elsif date_input.to_i > @events.length
             puts "Please enter a number from the list."
+          elsif date_input == "list"
+            list_events_by_date
           elsif date_input == "back"
           
           else
             puts "I did not understand your selection."
           end
         end
+        
       when "2"
         puts <<-DOC
           1. Standard
@@ -68,13 +82,14 @@ class MtgEventInfo::CLI
               end
             end
           end
+          
       when "3"
         @events.each.with_index(1) do |event, i|
           puts "#{i}. #{event[:location]}"
         end
         location_input = nil
         while location_input != "back"
-          puts "Please enter the number corresponding to the loction for which you would like to display upcoming events or enter 'back' to return to the previous menu."
+          puts "Please enter the number corresponding to the location for which you would like to display upcoming events or enter 'back' to return to the previous menu."
           location_input = gets.strip.downcase
           if location_input.to_i > 0  && location_input.to_i <= @events.length
             puts "Well done."
@@ -86,13 +101,16 @@ class MtgEventInfo::CLI
             puts "I did not understand your selection."
           end
         end
+        
       else
         puts "I did not understand your selection."
       end
+      
     end
+    
   end
   
   def goodbye
-    puts "Thank you for using the Magic: The Gathering Event Information tool."
+    puts "Thank you for using Mox Ruby, a Magic: The Gathering Event Information tool."
   end
 end
