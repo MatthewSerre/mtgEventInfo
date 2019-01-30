caeb8b0aa86b265b819b3362420b037ca3cb80b8
matthew.serre <matthew.serre@gmail.com>
2019-01-30 03:21:17 +0000

    Fixed a few navigation errors within CLI.

--- lib/mtgEventInfo/CLI.rb
+++ lib/mtgEventInfo/CLI.rb
@@ -21,44 +21,7 @@ class MtgEventInfo::CLI
         
       when "2"
         list_events(2)
-        sub_input = nil
-        list_events_with_details(sub_input)
-        # while format_input != "back"
-        #   format_input = gets.strip.downcase
-        #   if format_input.to_i > 0 && format_input.to_i <= format_array.length
-        #     list_events_by_format(format_array, format_input)
-        #     nested_input = nil
-        #     while nested_input != "back"
-        #       puts "Please enter the number corresponding to the event about which you would like more information or enter 'list' to see the list of events again or enter 'back' to return to the main menu."
-        #       nested_input = gets.strip.downcase
-        #       if nested_input.to_i > 0  && nested_input.to_i <= @event_array.length
-        #         puts ""
-        #         puts "#{@event_array[nested_input.to_i-1][:location]} #{@event_array[:TO]} #{@event_array[nested_input.to_i-1][:mtgFormat]} #{@event_array[nested_input.to_i-1][:name]}"
-        #         puts @event_array[nested_input.to_i-1][:date]
-        #         puts @event_array[nested_input.to_i-1][:moreInfoURL]
-        #         puts ""
-        #       elsif nested_input.to_i > @event_array.length
-        #         puts "Please enter a number from the list."
-        #       elsif nested_input == "list"
-        #         list_events_by_format(format_array, format_input)
-        #       elsif nested_input == "back"
-              
-        #       else
-        #         puts "I did not understand your selection."
-        #       end
-        #     end
-            
-        #   elsif format_input.to_i > format_array.length
-        #     puts "Please enter a number from the list."
-        #   elsif format_input == "formats"
-        #     list_formats
-        #   elsif format_input == "back"
-        
-        #   else
-        #     puts "I did not understand your selection."
-        #   end
-        # end
-          
+
       when "3"
         list_events(3)
         sub_input = nil
@@ -75,11 +38,14 @@ class MtgEventInfo::CLI
   end
 
   def list_events_by
-    puts <<-DOC
-      1. Date
-      2. Format
-      3. Location
-    DOC
+    i = 0
+    @options = ["Date","Format","Location"]
+    puts ""
+    @options.each do |option|
+      i+=1
+      puts "#{i}. #{option}"
+    end
+    puts ""
     puts "Please enter the number corresponding to the option by which you want to sort and display upcoming events or enter 'exit'."
     @events = MtgEventInfo::Event.all
   end
@@ -93,24 +59,28 @@ class MtgEventInfo::CLI
       end
       
     elsif selection === 2
-      list_formats
       selection = nil
       while selection != "back"
+        list_formats
         puts "Please enter the number corresponding to the format by which you want to sort and display upcoming events."
-        selection = gets.strip.downcase
-        if selection.to_i > 0  && selection.to_i <= @format_array.length
+        @format_selection = gets.strip.downcase
+        if @format_selection.to_i > 0  && @format_selection.to_i <= @format_array.length
           i = 0
+          puts ""
           @events.each.with_index do |event|
-            if @format_array[selection.to_i-1] === event[:mtgFormat]
+            if @format_array[@format_selection.to_i-1] === event[:mtgFormat]
               i+=1
               puts "#{i}. #{event[:date]} - #{event[:TO]} #{event[:name]} - #{event[:location]}"
             end
           end
-        elsif selection.to_i > @format_array.length
+          puts ""
+          sub_input = nil
+          list_events_with_details(sub_input)
+        elsif @format_selection.to_i > @format_array.length
           puts "Please enter a number from the list."
-        elsif selection == "list"
-          list_events(@input.to_i)
-        elsif selection == "back"
+        elsif @format_selection == "list"
+          list_events(@format_selection.to_i)
+        elsif @format_selection == "back"
         
         else
           puts "I did not understand your selection."
@@ -144,7 +114,11 @@ class MtgEventInfo::CLI
       elsif selection.to_i > @events.length
         puts "Please enter a number from the list."
       elsif selection == "list"
-        list_events(@input.to_i)
+        if @input.to_i === 1 || @input.to_i === 3
+          list_events(@input.to_i)
+        else
+          list_events(@format_selection.to_i)
+        end
       elsif selection == "back"
       
       else
@@ -158,7 +132,7 @@ class MtgEventInfo::CLI
     @format_array = ["Standard","Limited","Modern","Legacy","Team Constructed"]
     i = 0
     puts ""
-    @format_array.each_with_index do |mtgFormat|
+    @format_array.each do |mtgFormat|
       i+=1
       puts "#{i}. #{mtgFormat}"
     end
